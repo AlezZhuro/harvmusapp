d7b1e7a5d1a49fcddc61b881a5120113c2f6fce2
alezzhuro <alezzhuro@gmail.com>
2023-06-21 16:13:44 +0300

    init repo

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function App(): JSX.Element {
+  const backgroundStyle = {};
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Text>HI</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
